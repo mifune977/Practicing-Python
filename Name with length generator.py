cc4a4ec0606c951eb157178345a0711c1707bc00
mifune977 <142242019+mifune977@users.noreply.github.com>
2023-10-19 01:03:31 +0600

    Update Name with length generator.py

--- Name with length generator.py
+++ Name with length generator.py
@@ -14,3 +14,9 @@ print("hola " + name)
 print("Your name has")
 print(length)
 print("characters")
+
+# more shorter version
+num_char = len(input("What's your name?\n"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+

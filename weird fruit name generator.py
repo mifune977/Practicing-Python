f7d7a2a0c014a3a5e9afd0a5a5693da3ec5994a1
mifune977 <142242019+mifune977@users.noreply.github.com>
2023-10-18 19:57:24 +0600

    Update weird fruit name generator.py

--- weird fruit name generator.py
+++ weird fruit name generator.py
@@ -1,11 +1,11 @@
-# 1. Create a greeting for your program.
+# Create a greeting for your program.
 print('Welcome to the "Is it possible fruit generator"')
-# 2. Ask the user for his favourite color.
+# Ask the user for his favourite color.
 color = input("What is your favourite color? \n")
-# 3. Ask the user what's his/her's favourite fruit.
+# Ask the user what's his/her's favourite fruit.
 fruit = input("What's your favourite fruit? \n")
-# 4. Combine the name of their color and fruit and show them their newly created weird fruit.
+# Combine the name of their color and fruit and show them their newly created weird fruit.
 print(color + " " + fruit + "! but where I can find them now,lol")
-# 5. Make sure the input cursor shows on a new line:
+# Make sure the input cursor shows on a new line:
 
 

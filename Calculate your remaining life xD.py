51d8531a52d7b5c4ea634911806eafe99a414c2b
mifune977 <142242019+mifune977@users.noreply.github.com>
2023-10-25 05:52:33 +0600

    Create Calculate your remaining life xD.py
    Don't believe this calculator, just die already.

---
+++ Calculate your remaining life xD.py
@@ -0,0 +1,12 @@
+# aah life is too short xD
+
+age = input("What is your current age?")
+remaining_days = (90 - int(age)) * 365
+remaining_weeks = (90 - int(age)) * 52
+remaining_years =  (90 - int(age))
+
+print(f"You have left {remaining_days} days, {remaining_weeks} weeks & {remaining_years} years if you live a 90 years life. \nBest enjoy now, \nBecause in the long run, WE ALL DEAD!")
+
+
+
+

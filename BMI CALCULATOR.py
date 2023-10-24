0dbdaae9f40dff60012b21b3ec29b84a9a194fa6
mifune977 <142242019+mifune977@users.noreply.github.com>
2023-10-25 04:04:47 +0600

    Update BMI CALCULATOR.py
    round the results into two figure

--- BMI CALCULATOR.py
+++ BMI CALCULATOR.py
@@ -1,7 +1,7 @@
-height = input("What's your height in meter: ")
-weight = input("What's your weight in kg: ")
+height = input("What's your height in meter: \n")
+weight = input("What's your weight in kg: \n")
 
 w = float(weight)
 h = float(height)
 
-print("your BMI value is: " + str(w/(h**2)))
+print("your BMI value is: \n" + str(round(w/(h**2), 2)))

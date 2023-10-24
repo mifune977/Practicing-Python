a842640094c7da4727b334d11f1b2ea5a2a933a3
mifune977 <142242019+mifune977@users.noreply.github.com>
2023-10-25 05:28:44 +0600

    Create f string.py
    showcase of the use of f srting

---
+++ f string.py
@@ -0,0 +1,6 @@
+# showcase of f-string
+money = "$75,000"
+height = 1.78
+hungry = True
+
+print(f"hey your current status is below:\nyour current balance is: {money}\nbtw your height is {height}\nand finally are you really currently hungry?\n{hungry}")

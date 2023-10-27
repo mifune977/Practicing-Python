028c275688e6e2da1da1325b0318c4461c22cd2c
mifune977 <142242019+mifune977@users.noreply.github.com>
2023-10-28 03:58:35 +0600

    Update Calculate your remaining life xD.py

--- Calculate your remaining life xD.py
+++ Calculate your remaining life xD.py
@@ -1,12 +1,10 @@
-# aah life is too short xD
+# your life calculation if you live 90 years.
 
-age = input("What is your current age?")
+age = input("What's your current age?\n")
+
 remaining_days = (90 - int(age)) * 365
 remaining_weeks = (90 - int(age)) * 52
-remaining_years =  (90 - int(age))
+remaining_years = (90 - int(age)) * 1
 
-print(f"You have left {remaining_days} days, {remaining_weeks} weeks & {remaining_years} years if you live a 90 years life. \nBest enjoy now, \nBecause in the long run, WE ALL DEAD!")
-
-
-
+print(f"if you live 90 years then you have left, \n{remaining_days} days, \n{remaining_weeks} weeks \n& {remaining_years} years my friend.\n\nBest enjoy now, \nBecause in the long run, WE ALL DEAD!")
 
